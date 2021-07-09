6b9c193f66269dbdc7f8b6f6bbb0a7d3d3571cd2
fuzh <47854927+fuzh97@users.noreply.github.com>
2021-07-09 16:14:21 +0800

    Update 0198.打家劫舍.cpp

--- code/0198.打家劫舍.cpp
+++ code/0198.打家劫舍.cpp
@@ -26,4 +26,20 @@ class Solution {
         return ans;
     }
 };
+//空间优化
+class Solution {
+   public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = max(nums[0], nums[1]), len = nums.size();
+        for (int i = 2; i < len; i++) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+};
 // @lc code=end
