5f5bc6c99f3fc67d2943f4b3ef608f17ec805986
fuzh <47854927+fuzh97@users.noreply.github.com>
2021-07-13 09:53:02 +0800

    Update 0040.组合总和-ii.cpp

--- code/0040.组合总和-ii.cpp
+++ code/0040.组合总和-ii.cpp
@@ -5,26 +5,36 @@
  */
 
 // @lc code=start
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
 class Solution {
    public:
     vector<int> temp;
-    vector<vector<int>> ans;
-    void dfs(vector<int> &candidates, int target, int index) {
-        if (target == 0) {
+    void dfs(vector<vector<int>>& ans, vector<int>& candidates, int index, int target, int sum) {
+        if (sum == target) {
             ans.push_back(temp);
             return;
         }
-        for (int i = index; i < candidates.size() && target - candidates[i] >= 0;
-             i++) {
-            if (i > index && candidates[i] == candidates[i - 1]) continue;  //去重
+        if (sum > target || index == candidates.size()) {
+            return;
+        }
+        for (int i = index; i < candidates.size() && sum <= target; i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                //这个数字在这一轮已经用过了，跳过
+                continue;
+            }
             temp.push_back(candidates[i]);
-            dfs(candidates, target - candidates[i], i + 1);
-            temp.pop_back();  //回溯
+            dfs(ans, candidates, i + 1, target, sum + candidates[i]);
+            temp.pop_back();
         }
     }
-    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        dfs(candidates, target, 0);
+        vector<vector<int>> ans;
+        dfs(ans, candidates, 0, target, 0);
         return ans;
     }
 };
